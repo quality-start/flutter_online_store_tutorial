34e66c63a63ab20401d7e907b4aab05073629d6d
momiji.kusano <kusano.momiji@cloud-ace.jp>
2024-10-16 20:33:31 +0900

    余分なカウンターを削除

--- lib/main.dart
+++ lib/main.dart
@@ -9,98 +9,62 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo',
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({required this.title, super.key});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class ProductsModel {
-  ProductsModel({
-    required this.title,
-    required this.memory,
-    required this.price,
-    required this.image,
-  });
-  final String title;
-  final String memory;
-  final double price;
-  final String image;
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
     final items = List.generate(90, (index) => '${index + 1}');
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white54,
-        title: Image.asset('images/cyber.png'),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.favorite),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.shopping_cart),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.person),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: const <Widget>[
-            ListTile(
-              title: Text('Home'),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white54,
+          title: Image.asset('images/cyber.png'),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.favorite),
+              onPressed: () {},
             ),
-            ListTile(
-              title: Text('About'),
+            IconButton(
+              icon: const Icon(Icons.shopping_cart),
+              onPressed: () {},
             ),
-            ListTile(
-              title: Text('Contact Us'),
-            ),
-            ListTile(
-              title: Text('Blog'),
+            IconButton(
+              icon: const Icon(Icons.person),
+              onPressed: () {},
             ),
           ],
         ),
-      ),
-      body: SafeArea(
-        child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            mainAxisSpacing: 8,
-            crossAxisSpacing: 8,
-            childAspectRatio: 9 / 16,
+        drawer: Drawer(
+          child: ListView(
+            children: const <Widget>[
+              ListTile(
+                title: Text('Home'),
+              ),
+              ListTile(
+                title: Text('About'),
+              ),
+              ListTile(
+                title: Text('Contact Us'),
+              ),
+              ListTile(
+                title: Text('Blog'),
+              ),
+            ],
           ),
-          itemCount: items.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Card(
-              child: Center(child: Text('${index + 1}')),
-            );
-          },
+        ),
+        body: SafeArea(
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 8,
+              crossAxisSpacing: 8,
+              childAspectRatio: 9 / 16,
+            ),
+            itemCount: items.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                child: Center(child: Text('${index + 1}')),
+              );
+            },
+          ),
         ),
       ),
     );
